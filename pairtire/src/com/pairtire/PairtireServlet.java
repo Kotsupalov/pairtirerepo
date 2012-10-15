6594bb7ca602be7cea8ab8f0bc75ff6e4818fcc3
Kotsupalov <Kotsupaloff@gmail.com>
2012-10-15 23:53:52 +0300

    My mess

---
+++ pairtire/src/com/pairtire/PairtireServlet.java
@@ -0,0 +1,13 @@
+package com.pairtire;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class PairtireServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
